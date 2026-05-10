b27d8cdab0deeddf77622a0f63ce4727b53bb7b8
Code-Dawg <whatsupcodedwag@gmail.com>
2026-05-10 23:10:21 +0545

    swaping it

---
+++ swaping.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+print("inital value","a=",a ,"b=",b)
+
+a,b=b,a
+
+print("final value","a=",a ,"b=",b)
+
---
+++ swaping2.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+print("inital value","a=",a ,"b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("Final  value","a=",a ,"b=",b)
